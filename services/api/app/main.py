0cfad41356ad06fe22b5a73230fb1afe941ce092
Nadège Lefort <nlefort@49.cerfrance.fr>
2026-03-14 14:16:06 +0100

    fix logger

--- services/api/app/main.py
+++ services/api/app/main.py
@@ -10,6 +10,11 @@ from .database import SessionLocal, engine, get_db
 from . import data_structure
 from .weather_utils import get_weather_features, get_calendar_features
 
+
+# Configuration des logs
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://mlflow:5000")
 
 try:
@@ -19,9 +24,6 @@ try:
 except Exception as e:
     logger.warning(f"Impossible de se connecter à MLflow ({e}). L'API continuera sans tracking.")
 
-# Configuration des logs
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
